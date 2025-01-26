94f84845c6469bec127af8de99a6dc926ca0c29e
Vinaykumarpolepalli <vinaykumarpolepalli@gmail.com>
2025-01-26 18:15:30 +0530

    Add files via upload

---
+++ RemoveArray.java
@@ -0,0 +1,17 @@
+package ImportantPrograms;
+
+public class RemoveArray {
+	public static void main(String[] args) {
+        int[] num = {25,14,56,15,36,56,77,18,29,49};
+        int arr= num.length;
+        int index=3;
+        for (int i = index;i<arr-1;i++) {
+        	num[i]=num[i+1];
+        }
+        arr--;
+        for(int i = 0;i<arr;i++) {
+        	System.out.print(num[i]+   " ,"  );
+        }
+	
+	}
+}
